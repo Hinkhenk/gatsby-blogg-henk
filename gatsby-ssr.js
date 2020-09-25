854add67acb8215d299a8b6562240adc1eba47c6
Henrik <55479521+Hinkhenk@users.noreply.github.com>
2020-09-25 13:03:22 +0200

    added prism

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,24 +1,38 @@
 import React from "react"
 import { createGlobalStyle, ThemeProvider } from "styled-components"
 import { MDXProvider } from "@mdx-js/react"
+import { preToCodeBlock } from "mdx-utils"
 import Theme from "./src/themes/theme"
-import { TableComponent } from "./src/components"
+import { Table, Code } from "./src/components"
+import "./language-tabs.css"
 
 const GlobalStyles = createGlobalStyle`
-*{
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-}
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
 
-body, html {
-    font-family: ${props => props.theme.fonts.main};
-    height: 100%;
-    background-color: ${props => props.theme.colors.light1};
-}
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+        height: 100%;
+        background-color: ${props => props.theme.colors.light1};
+    }
+
 `
+
 const components = {
-  table: TableComponent,
+  table: Table,
+  pre: preProps => {
+    const props = preToCodeBlock(preProps)
+    // if there's a codeString and some props, we passed the test
+    if (props) {
+      return <Code {...props} />
+    }
+    // it's possible to have a pre without a code in it
+    return <pre {...preProps} />
+  },
+  wrapper: ({ children }) => <>{children}</>,
 }
 
 export const wrapRootElement = ({ element }) => (
